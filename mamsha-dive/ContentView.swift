aff167080a6c644441c39619203128636b3ea446
Yao Xu <34262576+yx1441@users.noreply.github.com>
2022-05-26 03:21:49 +0400

    Initial Commit

---
+++ mamsha-dive/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mamsha-dive
+//
+//  Created by Yao Xu on 05/26/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
