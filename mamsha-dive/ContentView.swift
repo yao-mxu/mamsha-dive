961bd86a0959c1f783b46df259851b510226af00
Yao Xu <34262576+yx1441@users.noreply.github.com>
2022-05-26 05:45:01 +0400

    initial commit

--- mamsha-dive/ContentView.swift
+++ mamsha-dive/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Text("Welcome to Mamsha Delima").padding()
+        }
+    }
+
+struct OrderView: View {
+    var body: some View {
+        Text("Choose for yourself").padding()
+        }
+    }
+
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Main", systemImage: "list.dash")
+                }
+
+            OrderView()
+                .tabItem {
+                    Label("Choose", systemImage: "square.and.pencil")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
